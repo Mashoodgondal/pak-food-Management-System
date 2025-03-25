4ed60b08101c164574fd8198a8d6d1eb416894f2
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-03-25 23:08:00 +0500

    calculator updation

--- app/calculator/page.js
+++ app/calculator/page.js
@@ -87,3 +87,17 @@ export default Calculator;
     <div className="grid grid-cols-4 gap-3">
         {["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "C", "0", "=", "+"].map((btn) => (
             <button */}
+
+// <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-yellow-300 to-orange-300 p-5">
+
+// <div className="bg-gradient-to-r from-orange-400 to-yellow-400 bg-opacity-20 backdrop-blur-lg shadow-lg rounded-2xl p-5 w-full max-w-sm">
+//     <h2 className="text-center text-red-600 text-3xl font-bold mb-3">Calculator</h2>
+
+//     <div className="bg-white p-3 rounded-lg shadow-inner mb-4">
+//         <input type="text" value={input} readOnly className="w-full text-right text-gray-500 text-lg bg-transparent outline-none" />
+//         <div className="text-right text-gray-700 text-xl font-semibold">{result}</div>
+//     </div>
+
+//     <div className="grid grid-cols-4 gap-3">
+//         {["7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "C", "0", "=", "+"].map((btn) => (
+//             <button
