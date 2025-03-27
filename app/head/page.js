31b86ffece8702f8c3ae7b3e118374a2598e5aaf
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-03-27 21:47:09 +0500

    header setting

--- app/head/page.js
+++ app/head/page.js
@@ -12,3 +12,13 @@ const HeaderPage = () => {
     );
 };
 export default HeaderPage;
+
+
+{/* <header className=" relative py-4 px-4 lg:py-6  shadow-md rounded-b-lg  text-center"
+style={{ backgroundImage: "url('/images/banner2.jpg')" }}
+>
+
+<h1 className="text-2xl sm:text-4xl lg:text-3xl font-extrabold text-black drop-shadow-md">
+    Pak Dairy Ice Cream & Fish
+</h1>
+</header> */}
