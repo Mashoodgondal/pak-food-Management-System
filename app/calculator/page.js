a056765217be87eebdfafd148e7043877d5e2f7a
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-03-29 23:06:57 +0500

    commit

--- app/calculator/page.js
+++ app/calculator/page.js
@@ -71,3 +71,11 @@ export default Calculator;
 //         <div className="text-right text-gray-700 text-xl font-semibold">{result}</div>
 //     </div>
 
+{/* <header className=" relative py-4 px-4 lg:py-6  shadow-md rounded-b-lg  text-center"
+style={{ backgroundImage: "url('/images/banner2.jpg')" }}
+>
+
+<h1 className="text-2xl sm:text-4xl lg:text-3xl font-extrabold text-black drop-shadow-md">
+    Pak Dairy Ice Cream & Fish
+</h1>
+</header> */}
