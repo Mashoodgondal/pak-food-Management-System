0c828a1888b93efd8d8397226a718320eef515db
Mashoodgondal <mashoodgondal6146@gmail.com>
2025-03-29 23:04:57 +0500

    header arrangement

--- app/head/page.js
+++ app/head/page.js
@@ -31,3 +31,19 @@ style={{ backgroundImage: "url('/images/banner2.jpg')" }}
     Pak Dairy Ice Cream & Fish
 </h1>
 </header> */}
+{/* <header className=" relative py-4 px-4 lg:py-6  shadow-md rounded-b-lg  text-center"
+style={{ backgroundImage: "url('/images/banner2.jpg')" }}
+>
+
+<h1 className="text-2xl sm:text-4xl lg:text-3xl font-extrabold text-black drop-shadow-md">
+    Pak Dairy Ice Cream & Fish
+</h1>
+</header> */}
+{/* <header className=" relative py-4 px-4 lg:py-6  shadow-md rounded-b-lg  text-center"
+style={{ backgroundImage: "url('/images/banner2.jpg')" }}
+>
+
+<h1 className="text-2xl sm:text-4xl lg:text-3xl font-extrabold text-black drop-shadow-md">
+    Pak Dairy Ice Cream & Fish
+</h1>
+</header> */}
